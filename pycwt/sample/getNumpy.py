cc864f67e601b3825a30fb25c63ce83677314ec0
88rising88rising <1255432560@qq.com>
2024-06-06 18:16:00 +0800

    test

--- pycwt/sample/getNumpy.py
+++ pycwt/sample/getNumpy.py
@@ -4,7 +4,7 @@ import numpy as np
 # array = np.array([[1, 2, 3], [4, 5, 6]])
 date = [str(l.split()[0]) for l in open("univ_all.txt")]
 array = np.array(date)
-print(123)
+print(1234)
 
 # 将数组保存到.npy文件
 np.save('np_all.npy', array)
